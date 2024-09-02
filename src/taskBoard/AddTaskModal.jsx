1a5c457e69c3c293365cd00c33102a09bd054a44
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-09-03 01:05:50 +0600

    add task done

--- src/taskBoard/AddTaskModal.jsx
+++ src/taskBoard/AddTaskModal.jsx
@@ -1,42 +1,107 @@
-export default function AddTaskModal() {
+import { useState } from "react";
+
+const AddTaskModal = ({onSave}) => {
+
+  const [task, setTask] = useState( {
+    id: crypto.randomUUID(),
+    title: "",
+    description: "",
+    tags: [],
+    priority: "",
+    isFavorite: false,
+});
+
+const handleChange = (evt) => {
+    const name = evt.target.name;
+    let value = evt.target.value;
+    if (name === 'tags') {
+        value = value.split(",");
+    }
+    setTask({
+        ...task,
+        [name]: value
+    })
+}
   return (
     <>
-      {/* <div className="bg-black bg-opacity-70 h-full w-full z-10 absolute top-0 left-0"></div> */}
+    <div className="bg-black bg-opacity-70 h-full w-full z-10 absolute top-0 left-0"></div>
+      <form className="mx-auto my-10 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11 z-10 absolute top-1/4 left-1/3">
+        <h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
+          Add New Task
+        </h2>
 
-      <form>
-        <div className="flex">
-          <div className="relative overflow-hidden rounded-lg text-gray-50 md:min-w-[380px] lg:min-w-[440px]">
+        <div className="space-y-9 text-white lg:space-y-10">
+          <div className="space-y-2 lg:space-y-3">
+            <label htmlFor="title">Title</label>
             <input
-              type="search"
-              id="search-dropdown"
-              className="z-20 block w-full bg-gray-800 px-4 py-2.5 pr-10 focus:outline-none"
-              placeholder="Search Task"
+              className="block w-full rounded-md bg-[#2D323F] px-3 py-2.5"
+              type="text"
+              name="title"
+              value={task.title}
+              onChange={handleChange}
+              id="title"
               required
             />
-            <button
-              type="submit"
-              className="absolute right-2 top-0 h-full rounded-e-lg text-white md:right-3"
-            >
-              <svg
-                className="h-4 w-4"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                />
-              </svg>
-              <span className="sr-only">Search</span>
-            </button>
           </div>
+
+          <div className="space-y-2 lg:space-y-3">
+            <label htmlFor="description">Description</label>
+            <textarea
+              className="block min-h-[120px] w-full rounded-md bg-[#2D323F] px-3 py-2.5 lg:min-h-[180px]"
+              type="text"
+              name="description"
+              value={task.description}
+              onChange={handleChange}
+              id="description"
+              required
+            ></textarea>
+          </div>
+
+          <div className="grid-cols-2 gap-x-4 max-md:space-y-9 md:grid lg:gap-x-10 xl:gap-x-20">
+            <div className="space-y-2 lg:space-y-3">
+              <label htmlFor="tags">Tags</label>
+              <input
+                className="block w-full rounded-md bg-[#2D323F] px-3 py-2.5"
+                type="text"
+                name="tags"
+                value={task.tags}
+              onChange={handleChange}
+                id="tags"
+                required
+              />
+            </div>
+
+            <div className="space-y-2 lg:space-y-3">
+              <label htmlFor="priority">Priority</label>
+              <select
+                className="block w-full cursor-pointer rounded-md bg-[#2D323F] px-3 py-2.5"
+                name="priority"
+                value={task.priority}
+              onChange={handleChange}
+                id="priority"
+                required
+              >
+                <option value="">Select Priority</option>
+                <option value="low">Low</option>
+                <option value="medium">Medium</option>
+                <option value="high">High</option>
+              </select>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-16 flex justify-center lg:mt-20">
+          <button
+          onClick={()=> onSave(task)}
+            type="submit"
+            className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
+          >
+            Create new Task
+          </button>
         </div>
       </form>
     </>
   );
-}
+};
+
+export default AddTaskModal;
--- src/taskBoard/TaskBoard.jsx
+++ src/taskBoard/TaskBoard.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Search from "./Search";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
+import AddTaskModal from "./AddTaskModal";
 
 const TaskBoard = () => {
 	const defaultTask = {
@@ -13,16 +14,26 @@ const TaskBoard = () => {
 		'isFavorite': false
 	}
 	const [tasks, setTasks] = useState([defaultTask]);
+  const [showModal, setShowModal] = useState(false);
+
+  const hanldeAddTask =(task)=>{
+    setTasks([
+      ...tasks, task
+    ])
+    setShowModal(false)
+  }
+  
 
   return (
     <section className="mb-20" id="tasks">
+      {showModal && <AddTaskModal onSave={hanldeAddTask}></AddTaskModal>}
       <div className="container">
         <div className="p-2 flex justify-end">
           <Search></Search>
         </div>
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskAction></TaskAction>
+          <TaskAction addOnClick={()=> setShowModal(true)}></TaskAction>
           <TaskList tasks={tasks}></TaskList>
         </div>
 
