586f5eda071759bbb72609496e5b141429bfd227
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-09-03 00:04:47 +0600

    addModal

--- src/taskBoard/TaskBoard.jsx
+++ src/taskBoard/TaskBoard.jsx
@@ -1,25 +1,34 @@
+import { useState } from "react";
 import Search from "./Search";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
 
-
 const TaskBoard = () => {
-    return (
-        <section className="mb-20" id="tasks">
-		
-		<div className="container">
-		
-		<div className="p-2 flex justify-end">
-			<Search></Search>
-		</div>
-		
-			<div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-				<TaskAction></TaskAction>
-				<TaskList></TaskList>
-			</div>
-		</div>
-	</section>
-    );
+	const defaultTask = {
+		'id': crypto. randomUUID(),
+		'title': "learn javascript",
+		'description': 'react is a most popular library in javascript',
+		'tags': ['react', 'js', 'vue'],
+		'priority': 'high',
+		'isFavorite': false
+	}
+	const [tasks, setTasks] = useState([defaultTask]);
+
+  return (
+    <section className="mb-20" id="tasks">
+      <div className="container">
+        <div className="p-2 flex justify-end">
+          <Search></Search>
+        </div>
+
+        <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+          <TaskAction></TaskAction>
+          <TaskList tasks={tasks}></TaskList>
+        </div>
+
+      </div>
+    </section>
+  );
 };
 
 export default TaskBoard;
